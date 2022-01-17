554286fce3a80d5545842128062af49774e1ffbd
dengxuan <you@example.com>
2022-01-17 20:50:39 +0800

    实现 数组、字典、函数字段支持

--- src/excelexporter/exporter.py
+++ src/excelexporter/exporter.py
@@ -1,5 +1,4 @@
-from distutils.log import info
-from distutils.sysconfig import customize_compiler
+from dataclasses import field
 import os
 import json
 import hashlib
@@ -7,23 +6,23 @@ import xlwings as xw
 import glob
 
 from typing import Sequence
-
 from .logger import logger
 from toml import load, dump
 
 
 CFG = {
-    "settings":{
+    "settings": {
         "ignore_sheet_mark": "~",  # 带星号的sheet不会导出
-        "custom_generator": "", # 自定义加工脚本,
-        "completed_hook": "", # 导表结束后调用脚本
-        "input": "data", # 导入目录
-        "output":"dist", # 导出目录
-        "project_root":"../" # 导出到项目的路径，比如你的 output="../Godot项目/Scripts/Settings"，那么project_root="../Godot项目"
+        "custom_generator": "",  # 自定义加工脚本,
+        "completed_hook": "",  # 导表结束后调用脚本
+        "input": "data",  # 导入目录
+        "output": "dist",  # 导出目录
+        "project_root": "../",  # 导出到项目的路径，比如你的 output="../Godot项目/Scripts/Settings"，那么project_root="../Godot项目"
     }
 }
 
 MD5MAP = {}
+
 
 def get_md5(fname):
     hash_md5 = hashlib.md5()
@@ -32,16 +31,19 @@ def get_md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def load_md5map():
     local_md5map = os.path.abspath("md5.txt")
     if os.path.exists(local_md5map):
-        with open("md5.txt",'r') as f:
+        with open("md5.txt", "r") as f:
             MD5MAP = json.load(f)
+
 
 def save_md5map():
     local_md5map = os.path.abspath("md5.txt")
-    with open(local_md5map, 'w+', encoding='utf-8') as f:
+    with open(local_md5map, "w+", encoding="utf-8") as f:
         json.dump(MD5MAP, f, ensure_ascii=False, indent=4)
+
 
 def load_toml_config() -> dict:
     local_cfg_path = os.path.abspath("export.toml")
@@ -51,9 +53,11 @@ def load_toml_config() -> dict:
     else:
         logger.warning("没有配置文件，将使用内置默认配置")
 
+
 def create_toml_config():
-    with open("export.toml", 'w+') as f:
+    with open("export.toml", "w+") as f:
         dump(CFG, f)
+
 
 def row2str_arr(row):
     return [str(cell.value) for cell in row]
@@ -67,26 +71,26 @@ def on_completed():
     if on_complete:
         with open(on_complete) as f:
             code = f.read()
-            exec(code, globals(), {"CFG":CFG})
+            exec(code, globals(), {"CFG": CFG})
     else:
         completed_gd()
 
-def excel2dict(workbook:xw.Book) -> dict:
-    abspath:str = workbook.fullname
+
+def excel2dict(workbook: xw.Book) -> dict:
+    abspath: str = workbook.fullname
     abssource = os.path.abspath(CFG["settings"]["input"])
     absoutput = os.path.abspath(CFG["settings"]["output"])
-    ignore_sheet_mark = CFG["settings"]["ignore_sheet_mark"] 
+    ignore_sheet_mark = CFG["settings"]["ignore_sheet_mark"]
     abspath_no_ext = os.path.splitext(abspath)[0]
 
     if not abspath.startswith(abssource):
         logger.error(f"{abspath} 不是配置表目录下的配置")
         return
 
-    sheets:Sequence[xw.Sheet] = workbook.sheets
+    sheets: Sequence[xw.Sheet] = workbook.sheets
 
-
-    #过滤出不带*的表名
-    sheets = filter(lambda sheet:not sheet.name.startswith(ignore_sheet_mark), sheets)
+    # 过滤出不带*的表名
+    sheets = filter(lambda sheet: not sheet.name.startswith(ignore_sheet_mark), sheets)
 
     custom_generator = CFG["settings"]["custom_generator"]
     if custom_generator:
@@ -101,17 +105,18 @@ def excel2dict(workbook:xw.Book) -> dict:
             org_name, rename = sheet.name, None
 
         data = {}
-        row_values = sheet.range('A1').expand().raw_value
+        row_values = sheet.range("A1").expand().raw_value
         data["define"] = {
-            "types": row_values[0],
+            "type": row_values[0],
             "desc": row_values[1],
-            "name": row_values[2]
+            "name": row_values[2],
         }
         data["table"] = list(row_values[3:])
 
-        relative_path = os.path.join(abspath_no_ext.replace(abssource, ""), rename or org_name)
-        output = absoutput+relative_path
-
+        relative_path = os.path.join(
+            abspath_no_ext.replace(abssource, ""), rename or org_name
+        )
+        output = absoutput + relative_path
 
         output_dirname = os.path.dirname(output)
         if not os.path.exists(output_dirname):
@@ -119,20 +124,63 @@ def excel2dict(workbook:xw.Book) -> dict:
 
         logger.info(f"导出: {abspath} => {output}")
         if custom_generator:
-            exec(code, globals(), {"data":data, "output":output})
+            exec(code, globals(), {"data": data, "output": output})
         else:
             gen_godot(data, output)
 
     workbook.close()
 
 
-def gen_godot(data,output):
+def gen_godot(data, output):
     import pprint
     import textwrap
+
+    func_codes = []
+    func_names = []
+
+    def make_func(v, n, id):
+        func_name = f"{n}_{id}"
+
+        func_code = textwrap.dedent(
+            f"""
+static func {func_name}(args=[]):
+{textwrap.indent(v, "    ")}
+"""
+        )
+        func_codes.append(func_code)
+        func_names.append(func_name)
+        return func_name
+
+    converter = {
+        "": lambda v, n, id: v or 0,
+        "string": lambda v, n, id: str(v) if v else "",
+        "int": lambda v, n, id: int(str(v or 0).split(".")[0]),
+        "float": lambda v, n, id: float(str(v or 0)),
+        "bool": lambda v, n, id: v != "FALSE",
+        "array": lambda v, n, id: eval(f'[{v.replace("|",",")}]') if v else [],
+        "array_str": lambda v, n, id: ["%s" % e for e in v.split("|")] if v else [],
+        "array_bool": lambda v, n, id: [e != "FALSE" for e in v.split("|")]
+        if v
+        else [],
+        "dict": lambda v, n, id: eval(f'{{{v.replace("|",",")}}}') if v else {},
+        "function": lambda v, n, id: make_func(v, n, id) if v else "",
+    }
 
     # 表格数据脚本模板
     template = """
     extends Reference
+    class Function extends Reference:
+
+        var func_name
+        var script_path
+
+        func _init(script_path, func_name):
+            self.func_name = func_name
+            self.script_path = script_path
+
+        func call(args=[]):
+            var this_script = load(script_path)
+            return this_script.call(self.func_name, args)
 
     static func data():
         var None = null
@@ -142,84 +190,103 @@ def gen_godot(data,output):
         var data = \\
         {data}
         return data
+
+    {funcs}
     """
     template = textwrap.dedent(template)
 
     # 在这里面写你的加工逻辑
     field_names = data["define"]["name"]
+    field_types = data["define"]["type"]
     table = {}
     for row in data["table"]:
-        row_data = {}
-        for index,value in enumerate(row):
-            field_name = field_names[index]
-            row_data[field_name] = value
-        table[row_data['id']] = row_data # 我们规定第一个字段是ID字段
-    
-    code = template.format(data=pprint.pformat(table,indent=2))
 
+        id_type = field_types[0]
+        id_cvt = converter[id_type] if id_type in converter else converter[""]
+        id = id_cvt(row[0], None, None)
+
+        row_data = {}
+        for index, value in enumerate(row):
+            field_name = field_names[index]
+            field_type = field_types[index]
+            cvt = converter[field_type] if field_type in converter else converter[""]
+            row_data[field_name] = cvt(value, field_name, id)
+        table[row_data["id"]] = row_data  # 我们规定第一个字段是ID字段
+
+    funcs = textwrap.dedent("\n".join(func_codes))
+
+    code = template.format(data=pprint.pformat(table, indent=2), funcs=funcs)
+
+    project_root = CFG["settings"]["project_root"]
+    relpath = os.path.relpath(output, project_root).replace("\\", "/")
     code = textwrap.dedent(code)
-    
-    with open(output+".gd", 'w+') as f:
+
+    for func_name in func_names:
+        code = code.replace(
+            f"'{func_name}'", f"Function.new('res://{relpath}.gd','{func_name}')"
+        )
+
+    with open(output + ".gd", "w+") as f:
         f.write(code)
+
 
 def completed_gd():
     from glob import glob
     import os
     import textwrap
-    output_path = CFG['settings']['output']
-    settings_file_path = os.path.join(output_path,'Settings.gd')
+
+    output_path = CFG["settings"]["output"]
+    settings_file_path = os.path.join(output_path, "Settings.gd")
     project_root = CFG["settings"]["project_root"]
 
     lines = []
 
-    for path in glob(f'{output_path}/**/*.*', recursive=True):
+    for path in glob(f"{output_path}/**/*.*", recursive=True):
         if path == settings_file_path:
             logger.info("跳过 Settings.gd")
             continue
         basename = os.path.basename(path)
         setting_name = os.path.splitext(basename)[0]
-        relpath = os.path.relpath(path, project_root).replace("\\","/")
+        relpath = os.path.relpath(path, project_root).replace("\\", "/")
         lines.append(f"const {setting_name} = preload('res://{relpath}')")
 
     # 去掉缩进
-    code = textwrap.dedent("""
+    code = textwrap.dedent(
+        """
     class_name Settings
     extends Reference
 
     {refs_code}
-    """)
-    refs_code = '\n'.join(lines) 
+    """
+    )
+    refs_code = "\n".join(lines)
 
     code = code.format(refs_code=refs_code)
 
-    with open(settings_file_path, 'w') as f:
+    with open(settings_file_path, "w") as f:
         f.write(code)
 
 
-
-
-def gen_all(cwd:str="."):
+def gen_all(cwd: str = "."):
     os.chdir(cwd)
     load_toml_config()
     abssource = os.path.abspath(CFG["settings"]["input"])
-    app = xw.App(visible=False)
-    exts = [".xlsx", ".xls"]
-    for ext in exts:
-        full_paths = glob.glob(f"{abssource}/**/*{ext}",recursive=True)
-        for full_path in full_paths:
-            filename = os.path.basename(full_path)
-            if filename.startswith("~$"):
-                logger.warning(f"{filename} 不是配置表，跳过！")
-                continue
-            book = app.books.open(full_path)
-            excel2dict(book)
-    on_completed()
-    app.quit()
+    with xw.App(visible=False) as app:
+        exts = [".xlsx", ".xls"]
+        for ext in exts:
+            full_paths = glob.glob(f"{abssource}/**/*{ext}", recursive=True)
+            for full_path in full_paths:
+                filename = os.path.basename(full_path)
+                if filename.startswith("~$"):
+                    logger.warning(f"{filename} 不是配置表，跳过！")
+                    continue
+                book = app.books.open(full_path)
+                excel2dict(book)
+        on_completed()
 
 
-def gen_one(path, cwd:str="."):
-    app = xw.App(visible=False)
-    book = app.books.open(path)
-    excel2dict(book)
-    on_completed()
-    app.quit()
+def gen_one(path, cwd: str = "."):
+    with xw.App(visible=False) as app:
+        book = app.books.open(path)
+        excel2dict(book)
+        on_completed()
