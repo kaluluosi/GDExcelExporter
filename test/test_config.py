cf4fc62a762c2555b304835f0e00541aa83fe0f7
kaluluosi <kaluluosi@gamil.com>
2023-03-17 21:31:47 +0800

    重构配置文件读取模块

---
+++ test/test_config.py
@@ -0,0 +1,57 @@
+import os
+import unittest
+import tempfile
+
+import toml
+from excelexporter.config import Configuration
+
+
+class TestConfig(unittest.TestCase):
+
+    def test_create_config(self):
+
+        config = Configuration()
+
+        config = Configuration(
+            ignore_sheet_mark='!',
+            ignore_field_mark='*',
+            custom_generator='1',
+            completed_hook='2',
+            input='a',
+            output='b',
+            project_root='c',
+        )
+
+        config.ignore_field_mark = "#"
+
+    def test_save_load(self):
+
+        config = Configuration()
+
+        with tempfile.TemporaryDirectory() as dirname:
+            temp = os.path.join(dirname, "config.toml")
+            config.save(temp)
+            new_config: Configuration = Configuration.load(temp)
+            self.assertEqual(
+                config.ignore_field_mark,
+                new_config.ignore_field_mark
+            )
+
+    def test_load_wrong_format(self):
+
+        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp:
+            tmp.write("xxx")
+            tmp.seek(0)
+            self.assertRaises(
+                toml.TomlDecodeError,
+                Configuration.load,
+                tmp.name
+            )
+
+    def test_load_not_exist(self):
+
+        self.assertRaises(
+            FileNotFoundError,
+            Configuration.load,
+            "xxx.ee"
+        )
