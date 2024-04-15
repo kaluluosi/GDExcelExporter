bc7deaea5a9f42922108b72e8c8bd22d3801fa8b
kaluluosi111 <kaluluosi@gmail.com>
2024-04-15 21:42:53 +0800

    优化测试用例

--- ee.py
+++ ee.py
@@ -1,4 +1,3 @@
-import gd_excelexporter
 from gd_excelexporter.cli import cli
 
 if __name__ == "__main__":
---
+++ src/gd_excelexporter/__main__.py
@@ -0,0 +1,10 @@
+"""
+author:        kaluluosi111 <kaluluosi@gmail.com>
+date:          2024-04-15 21:31:12
+Copyright © Kaluluosi All rights reserved
+"""
+
+from gd_excelexporter.cli import cli
+
+if __name__ == "__main__":
+    cli()
